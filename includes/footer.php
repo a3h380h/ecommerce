63f311001c059ddbee97fbe39db134aa5432cdf2
a3h380h <106443853+a3h380h@users.noreply.github.com>
2022-06-01 01:22:02 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+  <div class="container">
+    <div class="pull-right hidden-xs">
+       <b>All rights reserved</b>
+</div>
+    <strong>Copyright &copy; christboro - 2021-2022</strong> 
+  <div>
+</footer>
